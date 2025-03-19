d456556fbb7fad0182e8df1592be0d63201c62f3
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-03-19 15:01:27 -0400

    created /volcanoes route and get() view functionality to handle get requests to retrieve all volcanoes

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
@@ -13,6 +13,14 @@ from models import Volcano
 
 
 # Views go here!
+
+class AllVolcanoes(Resource):
+    def get(self):
+        volcanoes = Volcano.query.all()
+        response_body = [volcano.to_dict(only=('id', 'location', 'image')) for volcano in volcanoes]
+        return make_response(response_body, 200)
+
+api.add_resource(AllVolcanoes, '/volcanoes')
 
 @app.route('/')
 def index():
