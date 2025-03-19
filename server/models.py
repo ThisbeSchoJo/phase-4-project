f4da15801577c06c46aff845f6ab26fdae7314b2
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-03-19 14:55:46 -0400

    Added Volcano model with columns, constraints, validation, and __repr__() method

--- server/models.py
+++ server/models.py
@@ -5,3 +5,21 @@ from sqlalchemy.orm import validates
 from config import db
 
 # Models go here!
+class Volcano(db.Model, SerializerMixin):
+    __tablename__ = 'volcanoes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+
+    @validates('location', 'image')
+    def validate_location(self, column_name, value):
+        if type(value) != str:
+            raise TypeError(f"{column_name} must be a string!")
+        elif len(value) < 4:
+            raise ValueError(f"{column_name} must be at least 5 characters long!")
+        else:
+            return value
+        
+    def __repr__(self):
+        return f"<Volcano {self.id} - Location: {self.location}, Image: {self.image}>"
--- server/seed.py
+++ server/seed.py
@@ -4,14 +4,24 @@
 from random import randint, choice as rc
 
 # Remote library imports
-from faker import Faker
+# from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Volcano
 
 if __name__ == '__main__':
-    fake = Faker()
+    # fake = Faker()
     with app.app_context():
+        # Delete current volcano data before seeding...
+        Volcano.query.delete()
+
+        # Begin seeding...
         print("Starting seed...")
-        # Seed code goes here!
+
+        volcano_1 = Volcano(location="Pompeii", image="https://as2.ftcdn.net/v2/jpg/05/30/53/17/1000_F_530531798_yJOJBlxketxdUv1axTObn0WVWY0lBre5.jpg")
+        volcano_2 = Volcano(location="Hawaii", image="https://www.gannett-cdn.com/presto/2019/03/14/USAT/202d1217-b3d4-4dee-bc6b-9af009773203-AFP_AFP_1665JS.JPG?crop=2447,1376,x0,y897&width=3200&height=1680&fit=bounds")
+
+        db.session.add(volcano_1, volcano_2)
+        db.session.commit()
+        print("Volcanoes successfully succeeded!")
