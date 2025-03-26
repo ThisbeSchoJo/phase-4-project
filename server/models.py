e5d7316b9f7343c66d7394c3917ca6cc3142ba2f
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-03-26 17:02:33 -0400

    added DyeMaterial class to models.py and imported validates into models.py and added validation for name, base_color, and image to all be a string and at least 5 characters long

--- server/models.py
+++ server/models.py
@@ -5,14 +5,15 @@ from sqlalchemy.orm import validates
 from config import db
 
 # Models go here!
-class Volcano(db.Model, SerializerMixin):
-    __tablename__ = 'volcanoes'
+class DyeMaterial(db.Model, SerializerMixin):
+    __tablename__ = 'dyematerials'
 
     id = db.Column(db.Integer, primary_key=True)
-    location = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=True)
+    base_color = db.Column(db.String, nullable=True)
     image = db.Column(db.String, nullable=False)
 
-    @validates('location', 'image')
+    @validates('name', 'base_color', 'image')
     def validate_location(self, column_name, value):
         if type(value) != str:
             raise TypeError(f"{column_name} must be a string!")
@@ -22,4 +23,4 @@ class Volcano(db.Model, SerializerMixin):
             return value
         
     def __repr__(self):
-        return f"<Volcano {self.id} - Location: {self.location}, Image: {self.image}>"
+        return f"<Dye Material {self.id} - Name: {self.name}, Base Color: {self.base_color}, Image: {self.image}>"
