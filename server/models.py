e31eb57085489ff16b8134f86f090bc99083089b
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-03-26 17:37:55 -0400

    added Mordant and DyeResultmodels to models.py and added validations and repr messages for both

--- server/models.py
+++ server/models.py
@@ -6,12 +6,14 @@ from config import db
 
 # Models go here!
 class DyeMaterial(db.Model, SerializerMixin):
-    __tablename__ = 'dyematerials'
+    __tablename__ = 'dye_materials'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=True)
-    base_color = db.Column(db.String, nullable=True)
+    name = db.Column(db.String, nullable=False)
+    base_color = db.Column(db.String, nullable=False)
     image = db.Column(db.String, nullable=False)
+
+    dye_results = db.relationship('DyeResult', back_populates='dye_material')
 
     @validates('name', 'base_color', 'image')
     def validate_location(self, column_name, value):
@@ -24,3 +26,51 @@ class DyeMaterial(db.Model, SerializerMixin):
         
     def __repr__(self):
         return f"<Dye Material {self.id} - Name: {self.name}, Base Color: {self.base_color}, Image: {self.image}>"
+    
+
+class Mordant(db.Model, SerializerMixin):
+    __tablename__ = 'mordants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    effect = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+
+    dye_results = db.relationship('DyeResult', back_populates='mordant')
+
+    @validates('name', 'effect', 'image')
+    def validate_location(self, column_name, value):
+        if type(value) != str:
+            raise TypeError(f"{column_name} must be a string!")
+        elif len(value) < 4:
+            raise ValueError(f"{column_name} must be at least 5 characters long!")
+        else:
+            return value
+        
+    def __repr__(self):
+        return f"<Mordant {self.id} - Name: {self.name}, Effect: {self.effect}, Image: {self.image}>"
+
+
+class DyeResult(db.Model, SerializerMixin):
+    __tablename__ = 'dye_results'
+
+    id = db.Column(db.Integer, primary_key=True)
+    dye_material_id = db.Column(db.Integer, db.ForeignKey('dye_materials.id'), nullable=False)
+    mordant_id = db.Column(db.Integer,db.ForeignKey('mordants.id'), nullable=False)
+    resulting_color = db.Column(db.String, nullable=False)
+    intensity = db.Column(db.Integer, nullable=False)
+
+    dye_material=db.relationship('DyeMaterial', back_populates='dye_results')
+    mordant = db.relationship('Mordant', back_populates='dye_results')
+
+    @validates('name', 'effect', 'image')
+    def validate_location(self, column_name, value):
+        if type(value) != str:
+            raise TypeError(f"{column_name} must be a string!")
+        elif len(value) < 4:
+            raise ValueError(f"{column_name} must be at least 5 characters long!")
+        else:
+            return value
+        
+    def __repr__(self):
+        return f"<Mordant {self.id} - Name: {self.name}, Effect: {self.effect}, Image: {self.image}>"
