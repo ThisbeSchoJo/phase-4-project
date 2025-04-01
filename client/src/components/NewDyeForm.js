93234d93b16a007e2d53577fee73bc220e638720
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-04-01 13:30:58 -0400

    added functionality to NewDyeForm() with useState, useNavigate, and useOutletContext in NewDyeForm.js

--- client/src/components/NewDyeForm.js
+++ client/src/components/NewDyeForm.js
@@ -1,20 +1,60 @@
-
+import { useOutletContext, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function NewDyeForm(){
+    const { addDye } = useOutletContext()
+    const navigate = useNavigate()
+
+    const [formData, setFormData] = useState({
+        name: "",
+        base_color: "",
+        image: ""
+    })
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        fetch('/dye_materials', {
+            method: 'POST',
+            headers: {
+                "Content-Type" : "application/json",
+                "Accept" : "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(response => {
+            if (response.ok) {
+                response.json().then(newDyeData => {
+                    addDye(newDyeData) //Add the new dye material to the state
+                    navigate('/') //redict to home page
+                })
+            } else {
+                response.json().then(errorObject => {
+                    alert(`Error: ${errorObject.error}`)
+                })
+            }
+        })
+    }
+
+    function updateFormData(event){
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+
     return(
         <div>
             <h2>Add a new dye!</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="new-dye">Dye Material: </label>
-                <input type="text" id="new-dye" name="dye"/>
+                <input onChange={updateFormData} type="text" id="new-dye" name="name" value={formData.name}/>
                 <br></br>
                 <br></br>
                 <label htmlFor="new-base-color">Base Color: </label>
-                <input type="text" id="new-base-color" name="base-color"/>
+                <input onChange={updateFormData} type="text" id="new-base-color" name="base_color" value={formData.base_color}/>
                 <br></br>
                 <br></br>
                 <label htmlFor="new-image">Image URL: </label>
-                <input type="text" id="new-image" name="image" />
+                <input onChange={updateFormData} type="text" id="new-image" name="image" value={formData.image}/>
                 <br></br>
                 <br></br>
                 <input type="submit" value="Add Dye"/>
