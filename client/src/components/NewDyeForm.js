b0eff19f6f55c8e9f5d5514706fcdecc45237d1f
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-04-01 13:01:30 -0400

    added form display to NewDyeForm.js

--- client/src/components/NewDyeForm.js
+++ client/src/components/NewDyeForm.js
@@ -1,6 +1,25 @@
+
+
 function NewDyeForm(){
     return(
-        <h1>Add a new dye!</h1>
+        <div>
+            <h2>Add a new dye!</h2>
+            <form>
+                <label htmlFor="new-dye">Dye Material: </label>
+                <input type="text" id="new-dye" name="dye"/>
+                <br></br>
+                <br></br>
+                <label htmlFor="new-base-color">Base Color: </label>
+                <input type="text" id="new-base-color" name="base-color"/>
+                <br></br>
+                <br></br>
+                <label htmlFor="new-image">Image URL: </label>
+                <input type="text" id="new-image" name="image" />
+                <br></br>
+                <br></br>
+                <input type="submit" value="Add Dye"/>
+            </form>
+        </div>
     )
 }
 
