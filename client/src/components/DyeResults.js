c75fb063829b90c17847b33d12d583325d7e7891
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-04-06 13:17:32 -0400

    added to DyeResults component in DyeResults.js to display basic info

--- client/src/components/DyeResults.js
+++ client/src/components/DyeResults.js
@@ -5,6 +5,23 @@ function DyeResults() {
 
     return (
         <div>
+            <h2>Dye Results</h2>
+            <ul>
+                {dyeResults.map((result) => (
+                    <li>
+                        <div>
+                            <h3>Dye Material: {result.dye_material.name} + Mordant: {result.mordant.name}</h3>
+                            <p>Resulting Color: {result.resulting_color}</p>
+                            <p>Intensity: {result.intensity}</p>
+                            <div>
+                                <img src={result.dye_material.image} alt={result.dye_material.name} width={100} />
+                                <img src={result.mordant.image} alt={result.mordant.name} width={100} />
+                            </div>
+
+                        </div>
+                    </li>
+                ))}
+            </ul>
 
         </div>
     )
